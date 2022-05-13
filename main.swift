4eb6c1aa0acd692f8fd4e1cf6d0748411fc9d03d
Francisco Garache <franciscogarach3@gmail.com>
2022-05-13 21:37:54 +0000

    operadores logicos

--- main.swift
+++ main.swift
@@ -10,3 +10,36 @@ if a == b {
 }else{
   print("No son iguales")
 }
+
+1+2 //3
+5-3 //2
+2*3 //6
+10.0/2.5 //4
+
+"Hello " + "World" //hello word
+
+//D/d -> D == d*c+r
+9/4 //c = 2
+9%4 //r =1
+
+9 == 4*2+1 //true
+
+-9%4 // -9 == -2*4 + (-1)
+
+let five = 5 //5
+let minusFive = -five //-5
+let plusFive = -minusFive // 5
+
+var number = 5
+number += 3 //number = number + 3
+number -= 2 //number = number - 2
+
+1 == 1 //true
+1 == 2 //false
+1 != 2 //true
+2 > 1 //true
+2 < 1 //false
+2 >= 1 //true
+1 >= 1 //true
+2 <= 1 //false
+ 
